3baeb9d9ed6a9865bd5607f761dd9f867a24bdf4
Mohammad Za'areer <zaa92reer@gmail.com>
2021-05-18 20:28:40 +0300

    CC-06 solved

--- Data-Structures/linkedList/linked-list.js
+++ Data-Structures/linkedList/linked-list.js
@@ -44,9 +44,77 @@ class LinkedList {
         str += 'NULL';
         return str;
     }
+
+    append(value) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+    }
+    insertBefore(value, newVal) {
+        if (this.includes(value)) {
+            const node = new Node(newVal);
+            let previous = null;
+            let current = this.head;
+            while (current.value !== value) {
+                previous = current;
+                current = current.next;
+            }
+            if (!previous) {
+                node.next = current;
+                this.head = node;
+            } else {
+                node.next = current;
+                previous.next = node;
+            }
+        } else {
+            throw new Error(`${value} does not exist in the LL`);
+        }
+    }
+
+    insertAfter(value, newVal) {
+        if (this.includes(value)) {
+            const node = new Node(newVal);
+            let current = this.head;
+            while (current.value !== value) {
+                current = current.next;
+            }
+            node.next = current.next;
+            current.next = node;
+        } else {
+            throw new Error(`${value} does not exist in the LL`);
+        }
+    }
 }
 
 module.exports = {
     Node,
     LinkedList
 }
+
+// creating a new empty SLL
+const linkedList = new LinkedList();
+
+// insert a new node
+linkedList.insert(5);
+
+// append a new node
+linkedList.append(6);
+
+// insert before a node
+linkedList.insertBefore(5, 4);
+
+// insert after node
+linkedList.insertAfter(6, 3);
+
+// check if 9 is in the ll
+linkedList.includes(9);
+
+// return ll as formated string
+console.log(linkedList.toString());
--- README.md
+++
@@ -1,9 +0,0 @@
-# Code Challenges and Algorithms- 401 Course
-
-## Table of Contents  
-|No|Topic|
-|--|-----|
-|1|[ReverseArray](challenges/arrayReverse)|
-|2|[InsertShiftArray](challenges/arrayShift)|
-|3|[BinarySearch](challenges/arrayBinarySearch)|
-|4|[SLL implementation](Data-Structures/linkedList)|
