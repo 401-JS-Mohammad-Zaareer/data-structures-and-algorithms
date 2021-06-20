110220e79a16415d3a7c2aed8e3ebe0ac7a563a6
Mohammad Za'areer <zaa92reer@gmail.com>
2021-06-21 02:24:02 +0300

    CC-27 solved

--- __tests__/merge-sort.test.js
+++ __tests__/merge-sort.test.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const mergeSort = require('../challenges/mergeSort/merge-sort');
+
+describe('Testing mergeSort function', ()=>{
+    it('Sorts an array of integers in ascending order', ()=>{
+        const arr = [8,4,23,42,16,15];
+        const sortedArr = [ 4, 8, 15, 16, 23, 42 ];
+        mergeSort(arr);
+        expect(arr).toEqual(sortedArr);
+    });
+    it('Sorts an array that is reverse-sorted', ()=>{
+        const arr = [20,18,12,8,5,-2];
+        const sortedArr = [-2, 5, 8, 12, 18, 20];
+        mergeSort(arr);
+        expect(arr).toEqual(sortedArr);
+    });
+    it('Sorts an array with few uniques', ()=>{
+        const arr = [5,12,7,5,5,7];
+        const sortedArr = [ 5, 5, 5, 7, 7, 12 ];
+        mergeSort(arr);
+        expect(arr).toEqual(sortedArr);
+    });
+    it('Sorts an array that nearly sorted', ()=>{
+        const arr = [2,3,5,7,13,11];
+        const sortedArr = [ 2, 3, 5, 7, 11, 13 ];
+        mergeSort(arr);
+        expect(arr).toEqual(sortedArr);
+    });
+})
--- challenges/mergeSort/merge-sort.js
+++ challenges/mergeSort/merge-sort.js
@@ -1,1 +1,46 @@
 'use strict';
+
+function mergeSort(arr) {
+    const n = arr.length;
+
+    if(n > 1) {
+        const mid = Math.floor(n/2);
+        const left = arr.slice(0,mid);
+        const right = arr.slice(mid);
+        
+        mergeSort(left);
+        mergeSort(right);
+        merge(left, right, arr);
+    }
+}
+
+function  merge(left, right, arr) {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    while (i < left.length && j < right.length) {
+        if(left[i] <= right[j]) {
+            arr[k] = left[i];
+            i++;
+        } else {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    if (i == left.length) {
+        for(; k <arr.length; k++) {
+            arr[k] = right[j];
+            j++;
+        }
+    } else {
+        for(; k <arr.length; k++) {
+            arr[k] = left[i];
+            i++;
+        }
+    }
+}
+
+module.exports = mergeSort;
